a047fcfeec6d77504d797c63417972ffe4b328f4
Beck <beck.miller@utah.edu>
2024-03-04 13:28:01 -0700

    bug fixes in example run_SMART.py script

--- __SMART__/run_SMART.py
+++ __SMART__/run_SMART.py
@@ -5,27 +5,27 @@ sys.path.append(MODS)
 import SMART_probe_utils as SMART
 import SMART_conf_search as SMART_conf
 import SMART_descriptors as SMART_des
-#PROBES = os.path.join(os.getcwd(), 'Probes')
-
-#pt = Chem.GetPeriodicTable()
 
 file = 'R-TCPTTL_1.mol2'
+
 # Prepare structure
 structure = SMART.ReadFile(file)
-print(structure.NumAtoms)
-
+#print(structure.NumAtoms)
 structure.reference_vector(0, 1, dist=2.0)
-print(structure.Id)
 
 # Prepare probe
 probe = SMART.ReadProbe('S_SiF2_12_cyclic.mol2')
-probe_count = probe.NumAtoms
+#probe_count = probe.NumAtoms
+
 # Dock probe to structure
 print('\nadding probe',probe)
 docked = SMART.add_probe(structure, probe, dist=2.0)
 
+# Conf search (custom template method)
 cav, struc, cmplx = SMART_conf.CUSTOM_TEMPLATE_SEARCH(docked)
 #SMART.ExportStructure(docked, file.split('.')[0]+'_S_SiF2_8_cyclic')
+
+# Compute properties
 properties = SMART_des.get_all_properties(struc, cav, id, prox_radius=3.5, alpha=0)
 print(properties)
 print('DONE')
